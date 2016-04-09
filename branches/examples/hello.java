425b6b731c53e84f3c72d17626533a0dd5323bd1
srinivas <srinivasyyy@yahoo.com>
2016-04-09 13:48:22 -0700

    added examples hello.java

---
+++ branches/examples/hello.java
@@ -0,0 +1,25 @@
+ge examples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * Simple Hello servlet.
+  */
+
+  public final class Hello extends HttpServlet {
+
+
+      /**
+           * Respond to a GET request for the content produced by
+	        * this servlet.
+		     *
+		          * @param request The servlet request we are processing
+			       * @param response The servlet response we are producing
+			       "hello.java" 38L, 1115C
+			       
